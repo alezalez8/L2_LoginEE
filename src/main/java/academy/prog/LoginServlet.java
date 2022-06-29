f4d2cab52a46464eeb8012ea0c4ccee4fdecf8f0
alezalez8 <5446943@ukr.net>
2022-06-29 18:44:08 +0300

    homework 2 is done

--- src/main/java/academy/prog/LoginServlet.java
+++ src/main/java/academy/prog/LoginServlet.java
@@ -6,34 +6,28 @@ import java.io.IOException;
 
 public class LoginServlet extends HttpServlet {
     static final String LOGIN = "admin";
-    static final String PASS = "admin";
+    static final String PASS = "Administrator@";
     static final String MESSAGE_AGE = "Age must be more than 17 years and doesn't contain non digital symbols";
     static final String MESSAGE_LOGIN = "Login can't be less than 5 symbols";
     static final String MESSAGE_PASSWORD = "Password too short(can't be less than 10 symbols) or too simple";
-    static final int AGE = 18;
-    int age = 0;
-    private boolean isAge = false;
+    static final int MIN_AGE = 18;
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String login = request.getParameter("login");
         String password = request.getParameter("password");
         HttpSession session = request.getSession(true);
-
-
-        //if (LOGIN.equals(login) && PASS.equals(password))
-        //  if (PASS.equals(password)) {
+        int age = 0;
+        boolean isAge;
 
         try {
             age = Integer.parseInt(request.getParameter("age"));
         } catch (NumberFormatException e) {
             System.out.println(e);
-        } finally {
-            isAge = false;
         }
         //-----------------------------------
-        if (age < AGE) {
+        if (age < MIN_AGE) {
             session.setAttribute("age", false);
-            session.removeAttribute("user_login");
             session.setAttribute("message_age", MESSAGE_AGE);
             isAge = false;
         } else {
@@ -42,21 +36,24 @@ public class LoginServlet extends HttpServlet {
         }
         //-----------------------------------
         if (login.length() < 5) {
-
             session.setAttribute("my_login", false);
             session.removeAttribute("user_login");
             session.setAttribute("message_login", MESSAGE_LOGIN);
-
         } else {
             session.setAttribute("my_login", true);
         }
+        //-----------------------------------
 
-        if (LOGIN.equals(login) && PASS.equals(password) && isAge) {
-
-            session.setAttribute("user_login", login);
-
+        if (!isPass(password)) {
+            session.setAttribute("my_password", false);
+            session.setAttribute("message_password", MESSAGE_PASSWORD);
+        } else {
+            session.setAttribute("my_password", true);
         }
 
+        //-----------------------------------
+        if (LOGIN.equals(login) && PASS.equals(password) && isAge)
+            session.setAttribute("user_login", login);
         response.sendRedirect("index.jsp");
     }
 
@@ -64,10 +61,17 @@ public class LoginServlet extends HttpServlet {
         String a = request.getParameter("a");
         HttpSession session = request.getSession(false);
 
-        if ("exit".equals(a) && (session != null))
+        if ("exit".equals(a) && (session != null)) {
             session.removeAttribute("user_login");
-        isAge = false;
-
+        }
         response.sendRedirect("index.jsp");
+    }
+
+    private boolean isPass(String password) {
+        String pattern = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9@#$%]).{10,}";
+        if (password.matches(pattern)) {
+            return true;
+        }
+        return false;
     }
 }
